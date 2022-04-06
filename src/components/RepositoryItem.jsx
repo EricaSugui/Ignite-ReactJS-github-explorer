e7b058d2d96d7f7480f258174cd09eb32f1cbab8
EricaSugui <ericasugui@gmail.com>
2022-04-06 15:16:03 -0300

    adiciona mapeamento de repositórios no repositoryList

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,10 +1,10 @@
 export function RepositoryItem(props) {
   return (
     <li>
-      <strong>{props.repository?.name ?? 'default'}</strong>
-      <p>{props.repository.description}</p>
+      <strong>{props.repository?.name ?? 'Any name provided'}</strong>
+      <p>{props.repository?.description ?? 'Any description provided.'}</p>
 
-      <a href={props.repository.link}>Acessar repositório</a>
+      <a href={props.repository.html_url}>Acessar repositório</a>
     </li>
   );
 }
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,13 +3,6 @@ import { RepositoryItem } from "./RepositoryItem"
 
 import '../styles/repositories.scss'
 
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/EricaSugui'
-
-}
-
 export function RepositoryList() {
     const [repositories, setRepositories] = useState([]);
 
@@ -24,10 +17,11 @@ export function RepositoryList() {
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository}/>
+                })}
+
+
             </ul>
         </section>
     )
